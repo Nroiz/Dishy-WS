cdb9dd0d4c20f5b6c196bd028da2af3feea7089d
DanielShamama <daniel.shama@gmail.com>
2016-05-29 21:33:04 +0300

    add Authorization header allow

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ app.use(function (req, res, next) {
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 
     // Request headers
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Authorization');
     res.setHeader('Access-Control-Allow-Credentials', true);
     next();
 });
