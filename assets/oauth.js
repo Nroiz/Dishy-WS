6385eae7853a5543854c403ef2a40eb965025b7d
DanielShamama <daniel.shama@gmail.com>
2016-05-29 03:08:56 +0300

    auth callback path

--- assets/oauth.js
+++ assets/oauth.js
@@ -2,7 +2,7 @@ var ids = {
   google: {
     clientID: '620119103637-oamir1g7c7o42rjsrao46g47ngim0s2g.apps.googleusercontent.com',
     clientSecret: 'glWqt3fIJLF6s4n5lpoXdT9t',
-    callbackURL: 'http://fast-castle-79738.herokuapp.com/auth/google.callback'
+    callbackURL: 'http://fast-castle-79738.herokuapp.com/auth/google/callback'
   }
 };
 
