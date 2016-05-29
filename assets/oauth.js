2aaceeef2473ea809736ef561c0a1c84e0afc150
DanielShamama <daniel.shama@gmail.com>
2016-05-29 03:02:38 +0300

    auth callback path

--- assets/oauth.js
+++ assets/oauth.js
@@ -2,7 +2,7 @@ var ids = {
   google: {
     clientID: '620119103637-oamir1g7c7o42rjsrao46g47ngim0s2g.apps.googleusercontent.com',
     clientSecret: 'glWqt3fIJLF6s4n5lpoXdT9t',
-    callbackURL: 'http://127.0.0.1:1337/auth/google/callback'
+    callbackURL: 'http://mighty-dusk-71375.herokuapp.com/auth/google/callback'
   }
 };
 
